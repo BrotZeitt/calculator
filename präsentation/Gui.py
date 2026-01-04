7ef341635ca8e2e4fd7246e5a717a8af2b37b424
BrotZeitt <73233580+BrotZeitt@users.noreply.github.com>
2026-01-04 21:39:21 +0100

    hinzufügen des Eingabefelds und entfernen der vorherigen View Klasse

--- präsentation/Gui.py
+++ präsentation/Gui.py
@@ -25,6 +25,11 @@ class Gui(ttk.Frame):
         self.__berechnenButton = ttk.Button(self, text="Berechnen", command=controller.berechnen)
         self.__berechnenButton.pack()
 
+        eingabeFrame = ttk.Frame(self)
+        eingabeFrame.pack()
+
+        self.eingabebereich(eingabeFrame)
+
     def hohleZahl(self, zahl) -> float:
         match zahl:
             case 1:
@@ -42,3 +47,21 @@ class Gui(ttk.Frame):
         self.__ergebnis.config(text=str(ergebnis))
 
 
+    def eingabebereich(self, parent):
+        eingabefelder = [[["C", 1, "#fffff"], ["del", 2, "#fffff"], ["/", 1, "#fffff"]],
+                         [[7, 1, "#fffff"], [8, 1, "#fffff"], [9, 1, "#fffff"], ["*", 1, "#fffff"]],
+                         [[4, 1, "#fffff"], [5, 1, "#fffff"], [6, 1, "#fffff"], ["-", 1, "#fffff"]],
+                         [[1, 1, "#fffff"], [2, 1, "#fffff"], [3, 1, "#ffff"], ["+", 1, "#fffff"]],
+                         [[0, 2, "#fffff"], [",", 1, "#fffff"], ["=", 1, "#f21361"]]]
+        
+        for reihe in range(len(eingabefelder)):
+            spalte = 0
+            for feld in eingabefelder[reihe]:
+                text = feld[0]
+                breite = feld[1]
+                farbe = feld[2]
+
+                eingabeButton = ttk.Button(parent, text=text)
+                eingabeButton.grid(row=reihe, column=spalte, columnspan=breite, sticky="nsew")
+
+                spalte += breite
